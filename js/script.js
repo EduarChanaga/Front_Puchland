9e557b08b8de38781375665d807cfdfecc954bef
eduar <chanaga250@gmail.com>
2024-10-28 22:38:38 -0500

    feat: :sparkles: Avance pagina

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
-function toggleDropdown() {
-    document.getElementById("dropdownMenu").classList.toggle("show");
-}
 
-// Cierra el menú si se hace clic fuera de él
-window.onclick = function(event) {
-    if (!event.target.matches('.user-button') && !event.target.matches('.profile-pic')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+
+
+
+
+
